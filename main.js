e6faad35ba91cb32e1d711f042b18c70917fe65f
Sione Tenifa <sionetenifa@Siones-MacBook-Pro.local>
2020-11-03 08:46:06 -0700

    working but not pretty

--- main.js
+++ main.js
@@ -15,20 +15,22 @@ function createMeme(top, bottom, imgURL){
     console.log(top, bottom, imgURL)
     
      var ctx = mainCanvas.getContext("2d");
-    // // var img = document.getElementById("screamhttps://i.memeful.com/media/post/Wdn7mNw_700wa_0.gif");
-    // ctx.drawImage(imgURL, 10, 10)
 
     const background = new Image();
     background.src = imgURL;
     background.onload = function(){
-      ctx.drawImage(background, 0, 0);   
-    }
+        ctx.drawImage(background, 0, 0);   
+        ctx.textAlign = "center";
+        ctx.font = "50px Impact,sans-serif";
+        ctx.fillStyle = "white";
+        ctx.strokeStyle = "black";
+        ctx.lineWidth = "2";
+        ctx.fillText(top, 150, 40);
+        ctx.fillText(bottom, 150, 200)
+      }
+    
+}
+    
+
 
     
-    ctx.textAlign = "center";
-    ctx.font = "50px Impact,sans-serif";
-    // ctx.fillStyle = "white";
-    // ctx.strokeStyle = "black";
-    ctx.lineWidth = "2";
-    ctx.fillText(top, 10, 50);
-}
